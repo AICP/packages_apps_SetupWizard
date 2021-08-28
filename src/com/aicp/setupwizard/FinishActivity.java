0dca577ba85abbec46dec417453984404dc68c4b
Timi Rautamäki <timi.rautamaki@gmail.com>
2021-08-28 20:16:07 +0000

    SUW: FinishActivity: hide navigation bar & statusbar before animation
    Change-Id: Ia9d421a3f63469645f19acd91f72d472573e8818

--- src/com/aicp/setupwizard/FinishActivity.java
+++ src/com/aicp/setupwizard/FinishActivity.java
@@ -43,6 +43,7 @@ import android.view.View;
 import android.view.ViewAnimationUtils;
 import android.widget.ImageView;
 
+import com.google.android.setupcompat.util.SystemBarHelper;
 import com.google.android.setupcompat.util.WizardManagerHelper;
 
 import com.aicp.setupwizard.util.EnableAccessibilityController;
@@ -106,7 +107,7 @@ public class FinishActivity extends BaseSetupWizardActivity {
         sendBroadcastAsUser(i, getCallingUserHandle(), FINISH_SETUP);
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LOCKED);
-        hideNextButton();
+        SystemBarHelper.hideSystemBars(getWindow());
         finishSetup();
     }
 
