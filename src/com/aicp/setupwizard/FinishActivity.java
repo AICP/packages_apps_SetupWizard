LineageOS/android_packages_apps_SetupWizard

The big code style cleanup
* Format using AOSP code style in Android Studio,
  excluding `Rearrange entries` option.
* Rename static variables to use proper naming scheme
* Remove empty lines

Change-Id: I0d193e35f071bc288050e587b1693844d68865e1

src/org/lineageos/setupwizard/FinishActivity.java
@@ -17,6 +17,9 @@
 
 package org.lineageos.setupwizard;
 
+import static android.os.Binder.getCallingUserHandle;
+
+import static org.lineageos.setupwizard.Manifest.permission.FINISH_SETUP;
 import static org.lineageos.setupwizard.SetupWizardApp.ACTION_SETUP_COMPLETE;
 import static org.lineageos.setupwizard.SetupWizardApp.DISABLE_NAV_KEYS;
 import static org.lineageos.setupwizard.SetupWizardApp.ENABLE_RECOVERY_UPDATE;
@@ -35,8 +38,8 @@ import android.graphics.Bitmap;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.UserHandle;
 import android.os.SystemProperties;
+import android.os.UserHandle;
 import android.preference.PreferenceManager;
 import android.view.View;
 import android.view.ViewAnimationUtils;
@@ -47,9 +50,6 @@ import com.google.android.setupcompat.util.WizardManagerHelper;
 import org.lineageos.setupwizard.util.EnableAccessibilityController;
 
 import lineageos.providers.LineageSettings;
-
-import static android.os.Binder.getCallingUserHandle;
-import static org.lineageos.setupwizard.Manifest.permission.FINISH_SETUP;
 
 public class FinishActivity extends BaseSetupWizardActivity {
 
@@ -64,7 +64,6 @@ public class FinishActivity extends BaseSetupWizardActivity {
     private final Handler mHandler = new Handler();
 
     private volatile boolean mIsFinishing = false;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -162,10 +161,12 @@ public class FinishActivity extends BaseSetupWizardActivity {
             }
 
             @Override
-            public void onAnimationCancel(Animator animation) {}
+            public void onAnimationCancel(Animator animation) {
+            }
 
             @Override
-            public void onAnimationRepeat(Animator animation) {}
+            public void onAnimationRepeat(Animator animation) {
+            }
         });
         anim.start();
     }
@@ -191,7 +192,8 @@ public class FinishActivity extends BaseSetupWizardActivity {
         if (privacyData != null
                 && privacyData.containsKey(KEY_SEND_METRICS)) {
             LineageSettings.Secure.putInt(setupWizardApp.getContentResolver(),
-                    LineageSettings.Secure.STATS_COLLECTION, privacyData.getBoolean(KEY_SEND_METRICS)
+                    LineageSettings.Secure.STATS_COLLECTION,
+                    privacyData.getBoolean(KEY_SEND_METRICS)
                             ? 1 : 0);
         }
     }
@@ -216,8 +218,8 @@ public class FinishActivity extends BaseSetupWizardActivity {
         final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
 
         final boolean virtualKeysEnabled = LineageSettings.System.getIntForUser(
-                    context.getContentResolver(), LineageSettings.System.FORCE_SHOW_NAVBAR, 0,
-                    UserHandle.USER_CURRENT) != 0;
+                context.getContentResolver(), LineageSettings.System.FORCE_SHOW_NAVBAR, 0,
+                UserHandle.USER_CURRENT) != 0;
         if (enabled != virtualKeysEnabled) {
             LineageSettings.System.putIntForUser(context.getContentResolver(),
                     LineageSettings.System.FORCE_SHOW_NAVBAR, enabled ? 1 : 0,

AICP/packages_apps_SetupWizard

src/com/aicp/setupwizard/FinishActivity.java
@@ -17,6 +17,9 @@
 
 package com.aicp.setupwizard;
 
+import static android.os.Binder.getCallingUserHandle;
+
+import static com.aicp.setupwizard.Manifest.permission.FINISH_SETUP;
 import static com.aicp.setupwizard.SetupWizardApp.ACTION_SETUP_COMPLETE;
 import static com.aicp.setupwizard.SetupWizardApp.ENABLE_RECOVERY_UPDATE;
 import static com.aicp.setupwizard.SetupWizardApp.LOGV;
@@ -33,8 +36,8 @@ import android.graphics.Bitmap;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.UserHandle;
 import android.os.SystemProperties;
+import android.os.UserHandle;
 import android.preference.PreferenceManager;
 import android.view.View;
 import android.view.ViewAnimationUtils;
@@ -43,9 +46,6 @@ import android.widget.ImageView;
 import com.google.android.setupcompat.util.WizardManagerHelper;
 
 import com.aicp.setupwizard.util.EnableAccessibilityController;
-
-import static android.os.Binder.getCallingUserHandle;
-import static com.aicp.setupwizard.Manifest.permission.FINISH_SETUP;
 
 public class FinishActivity extends BaseSetupWizardActivity {
 
@@ -60,7 +60,6 @@ public class FinishActivity extends BaseSetupWizardActivity {
     private final Handler mHandler = new Handler();
 
     private volatile boolean mIsFinishing = false;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -158,10 +157,12 @@ public class FinishActivity extends BaseSetupWizardActivity {
             }
 
             @Override
-            public void onAnimationCancel(Animator animation) {}
+            public void onAnimationCancel(Animator animation) {
+            }
 
             @Override
-            public void onAnimationRepeat(Animator animation) {}
+            public void onAnimationRepeat(Animator animation) {
+            }
         });
         anim.start();
     }