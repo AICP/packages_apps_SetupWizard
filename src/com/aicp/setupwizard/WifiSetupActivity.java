LineageOS/android_packages_apps_SetupWizard

SUW: Cleanup intent extras
Remove unused extras:
allowSkip: Removed in 2015
wifi_auto_finish_on_connect: Removed in 2016

Use WizardManagerHelper definitions when possible

Change-Id: I4e6a503ae030b85486921fcacfc4fe03e37c8698

src/org/lineageos/setupwizard/WifiSetupActivity.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2016 The CyanogenMod Project
- * Copyright (C) 2017-2018,2020 The LineageOS Project
+ * Copyright (C) 2017-2021 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,6 +18,8 @@
 package org.lineageos.setupwizard;
 
 import static org.lineageos.setupwizard.SetupWizardApp.ACTION_SETUP_WIFI;
+import static org.lineageos.setupwizard.SetupWizardApp.EXTRA_PREFS_SET_BACK_TEXT;
+import static org.lineageos.setupwizard.SetupWizardApp.EXTRA_PREFS_SHOW_BUTTON_BAR;
 import static org.lineageos.setupwizard.SetupWizardApp.REQUEST_CODE_SETUP_WIFI;
 
 import android.content.Intent;
@@ -36,8 +38,8 @@ public class WifiSetupActivity extends WrapperSubBaseActivity {
         if (SetupWizardUtils.hasLeanback(this)) {
             intent.setComponent(SetupWizardUtils.mTvwifisettingsActivity);
         }
-        intent.putExtra(SetupWizardApp.EXTRA_PREFS_SHOW_BUTTON_BAR, true);
-        intent.putExtra(SetupWizardApp.EXTRA_PREFS_SET_BACK_TEXT, (String) null);
+        intent.putExtra(EXTRA_PREFS_SHOW_BUTTON_BAR, true);
+        intent.putExtra(EXTRA_PREFS_SET_BACK_TEXT, (String) null);
         startSubactivity(intent, REQUEST_CODE_SETUP_WIFI);
     }
 }

AICP/packages_apps_SetupWizard

src/com/aicp/setupwizard/WifiSetupActivity.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2016 The CyanogenMod Project
- * Copyright (C) 2017-2018,2020 The LineageOS Project
+ * Copyright (C) 2017-2021 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,6 +18,8 @@
 package com.aicp.setupwizard;
 
 import static com.aicp.setupwizard.SetupWizardApp.ACTION_SETUP_WIFI;
+import static com.aicp.setupwizard.SetupWizardApp.EXTRA_PREFS_SET_BACK_TEXT;
+import static com.aicp.setupwizard.SetupWizardApp.EXTRA_PREFS_SHOW_BUTTON_BAR;
 import static com.aicp.setupwizard.SetupWizardApp.REQUEST_CODE_SETUP_WIFI;
 
 import android.content.Intent;
@@ -36,8 +38,8 @@ public class WifiSetupActivity extends WrapperSubBaseActivity {
         if (SetupWizardUtils.hasLeanback(this)) {
             intent.setComponent(SetupWizardUtils.mTvwifisettingsActivity);
         }
-        intent.putExtra(SetupWizardApp.EXTRA_PREFS_SHOW_BUTTON_BAR, true);
-        intent.putExtra(SetupWizardApp.EXTRA_PREFS_SET_BACK_TEXT, (String) null);
+        intent.putExtra(EXTRA_PREFS_SHOW_BUTTON_BAR, true);
+        intent.putExtra(EXTRA_PREFS_SET_BACK_TEXT, (String) null);
         startSubactivity(intent, REQUEST_CODE_SETUP_WIFI);
     }
 }