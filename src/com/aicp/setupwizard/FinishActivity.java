LineageOS/android_packages_apps_SetupWizard

SUW: Move re-enabling of status bar to FinishActivity
This is always run both in the GMS and non-GMS flows

Change-Id: If70aa4cec640bacaf7b03da505b90d6c3c5aaf8e

src/org/lineageos/setupwizard/FinishActivity.java
@@ -49,6 +49,7 @@ import com.google.android.setupcompat.util.SystemBarHelper;
 import com.google.android.setupcompat.util.WizardManagerHelper;
 
 import org.lineageos.setupwizard.util.EnableAccessibilityController;
+import org.lineageos.setupwizard.util.SetupWizardUtils;
 
 import lineageos.providers.LineageSettings;
 
@@ -183,6 +184,7 @@ public class FinishActivity extends BaseSetupWizardActivity {
                 WallpaperManager.getInstance(mSetupWizardApp);
         wallpaperManager.forgetLoadedWallpaper();
         finishAllAppTasks();
+        SetupWizardUtils.enableStatusBar(this);
         Intent intent = WizardManagerHelper.getNextIntent(getIntent(),
                 Activity.RESULT_OK);
         startActivityForResult(intent, NEXT_REQUEST);

AICP/packages_apps_SetupWizard

src/com/aicp/setupwizard/FinishActivity.java
@@ -47,6 +47,7 @@ import com.google.android.setupcompat.util.SystemBarHelper;
 import com.google.android.setupcompat.util.WizardManagerHelper;
 
 import com.aicp.setupwizard.util.EnableAccessibilityController;
+import com.aicp.setupwizard.util.SetupWizardUtils;
 
 public class FinishActivity extends BaseSetupWizardActivity {
 
@@ -177,6 +178,7 @@ public class FinishActivity extends BaseSetupWizardActivity {
                 WallpaperManager.getInstance(mSetupWizardApp);
         wallpaperManager.forgetLoadedWallpaper();
         finishAllAppTasks();
+        SetupWizardUtils.enableStatusBar(this);
         Intent intent = WizardManagerHelper.getNextIntent(getIntent(),
                 Activity.RESULT_OK);
         startActivityForResult(intent, NEXT_REQUEST);